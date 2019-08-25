e31bc3bf17d5f92f26be191521dc5b03f441fac3
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 16:08:03 +0200

    Rename dice module to roll

--- nodejs/src/roll.js
+++ nodejs/src/roll.js
@@ -1,13 +1,21 @@
 const MAX_DICE_SIDES = 6;
 
 // returns an integer
-const roll = () => {
+const start = () => {
   // Returns a float number between 0 and not inclusive MAX_DICE_SIDES
   const pseudo = Math.random() * MAX_DICE_SIDES;
   // A step needs to be added to the floor to avoid 0 and reach 6
   return Math.ceil(pseudo);
 }
 
+const isOdd = (roll) => {
+  if (roll % 2 == 1) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = {
-  roll
+  start,
+  isOdd,
 };
