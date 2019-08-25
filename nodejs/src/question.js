14d6cb387b5683f70eb28e331d29ab3614559b3b
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 18:15:32 +0200

    QUESTION: Implement output emitter and isCorrect method

--- nodejs/src/question.js
+++ nodejs/src/question.js
@@ -1,8 +1,10 @@
+const { outputEmitter } = require('./output');
+
 const CATEGORIES = ['Pop', 'Science', 'Sports', 'Rock'];
 const NUMBER_CATEGORIES = CATEGORIES.length;
 const QUESTION = 'Question';
-const RIGHT_ANSWER_DOMAIN = 10;
-const RIGHT_ANSWER = 7;
+const ANSWER_DOMAIN = 10;
+const WRONG_ANSWER = 7;
 
 const choose = (place) => {
   const category = place % NUMBER_CATEGORIES;
@@ -13,17 +15,27 @@ const formulate = (category, index) => `${category} ${QUESTION} ${index}`;
 
 const ask = (place, count) => {
   const category = choose(place);
-  const index = count[category] + 1;
-  const question = formulate(category, index);
+  const index = count[category];
+
+  outputEmitter.emit('currentCategory', category);
+  outputEmitter.emit('askQuestion', formulate(category, index));
+
   return {
     index,
     category,
-    question,
   };
 }
 
-// The question is correct if a random number between 0 and 9 is equal to 7
-const isCorrect = () => Math.floor(Math.random() * RIGHT_ANSWER_DOMAIN) === RIGHT_ANSWER;
+// The question is wrong if a random number between 0 and 9 is equal to 7
+const isCorrect = () => {
+  const answer = Math.floor(Math.random() * ANSWER_DOMAIN); 
+  if (answer !== WRONG_ANSWER) {
+    outputEmitter.emit('correctAnswer');
+    return true;
+  }
+  outputEmitter.emit('wrongAnswer');
+  return false;
+}; 
 
 module.exports = {
   CATEGORIES,
