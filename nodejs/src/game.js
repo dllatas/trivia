189bd6aeca9cda95cd7dfc25ed53df690f11651f
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 21:18:07 +0200

    GAME: Move main function to index and remove test file

--- nodejs/src/game.js
+++ nodejs/src/game.js
@@ -4,7 +4,6 @@ const Question = require('./question');
 const { outputEmitter } = require('./output');
 
 const game = (players) => {
-
   if (!Player.enough(players)) {
     outputEmitter.emit('noPlayers');
     return;
@@ -15,8 +14,7 @@ const game = (players) => {
   const categoryCount = Question.generateCategoryCounter();
 
   while (notWinner) {
-    
-    for (let player of addedPlayers) {
+    for (const player of addedPlayers) {
       outputEmitter.emit('currentPlayer', player);
 
       const roll = Roll.start();
@@ -29,15 +27,16 @@ const game = (players) => {
           outputEmitter.emit('penaltyStay', player);
           continue;
         }
-      } 
+      }
 
       player.place = Player.move(player, roll);
 
       const { index, category } = Question.ask(player.place, categoryCount);
       categoryCount[category] = index + 1;
 
-      if (Question.isCorrect()) {
-        player.purse = player.purse + 1;
+      const answer = Question.getAnswer();
+      if (Question.isCorrect(answer)) {
+        player.purse += 1;
         outputEmitter.emit('coinsPlayer', player);
         if (Player.isWinner(player.purse)) {
           outputEmitter.emit('winner', player);
@@ -50,8 +49,8 @@ const game = (players) => {
       }
     }
   }
-}
+};
 
 module.exports = {
   game,
-}
+};
--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,5 +1,56 @@
-const { game } = require('./game');
+const Player = require('./player');
+const Roll = require('./roll');
+const Question = require('./question');
+const { outputEmitter } = require('./output');
+
+const game = (players) => {
+  if (!Player.enough(players)) {
+    outputEmitter.emit('noPlayers');
+    return;
+  }
+
+  let notWinner = true;
+  const addedPlayers = Player.add(players);
+  const categoryCount = Question.generateCategoryCounter();
+
+  while (notWinner) {
+    for (const player of addedPlayers) {
+      outputEmitter.emit('currentPlayer', player);
+
+      const roll = Roll.start();
+
+      if (player.penalty) {
+        if (Roll.isOdd(roll)) {
+          outputEmitter.emit('penaltyOut', player);
+          player.penalty = false;
+        } else {
+          outputEmitter.emit('penaltyStay', player);
+          continue;
+        }
+      }
+
+      player.place = Player.move(player, roll);
+
+      const { index, category } = Question.ask(player.place, categoryCount);
+      categoryCount[category] = index + 1;
+
+      const answer = Question.getAnswer();
+      if (Question.isCorrect(answer)) {
+        player.purse += 1;
+        outputEmitter.emit('coinsPlayer', player);
+        if (Player.isWinner(player.purse)) {
+          outputEmitter.emit('winner', player);
+          notWinner = false;
+          break;
+        }
+      } else {
+        player.penalty = true;
+        outputEmitter.emit('penaltySent', player);
+      }
+    }
+  }
+};
 
 module.exports = {
-  game
-}
+  game,
+};
