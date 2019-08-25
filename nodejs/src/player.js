19ff6c36da2318c40e892ad9fb43f4884f6fd414
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 15:52:16 +0200

    Add move function

--- nodejs/src/player.js
+++ nodejs/src/player.js
@@ -1,4 +1,5 @@
 const MINIMUM_PLAYERS_PER_GAME = 2; 
+const MAX_PLAYER_PLACE = 12; 
 
 // add a player or a set of players to the game
 const add = (playerList = []) => {
@@ -8,9 +9,17 @@ const add = (playerList = []) => {
       name: player,
       place: 0,
       purse: 0,
-      inPenaltyBox: false
+      penalty: false
     };
   });
+}
+
+const move = (place, roll) => {
+  next = place + roll;
+  if (next >= MAX_PLAYER_PLACE) {
+    next = next - MAX_PLAYER_PLACE;
+  }
+  return next;
 }
 
 // check that the number of players is at least two before starting the game
@@ -24,10 +33,10 @@ const enoughPlayers = (current = []) => {
   }
 
   return true;
-}
-
+} 
 
 module.exports = {
   add,
+  move,
   enoughPlayers,
 }
