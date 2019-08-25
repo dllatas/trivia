e44e6d789b0b8afb0133d64b8a3b07e223c809f2
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 18:35:36 +0200

    ROLL: Implement output emitter and improve comparison in isOdd method

--- nodejs/src/roll.js
+++ nodejs/src/roll.js
@@ -1,3 +1,5 @@
+const { outputEmitter } = require('./output');
+
 const MAX_DICE_SIDES = 6;
 
 // returns an integer
@@ -5,11 +7,13 @@ const start = () => {
   // Returns a float number between 0 and not inclusive MAX_DICE_SIDES
   const pseudo = Math.random() * MAX_DICE_SIDES;
   // A step needs to be added to the floor to avoid 0 and reach 6
-  return Math.ceil(pseudo);
+  const roll = Math.ceil(pseudo);
+  outputEmitter.emit('roll', roll);
+  return roll;
 }
 
 const isOdd = (roll) => {
-  if (roll % 2 == 1) {
+  if (roll % 2 === 1) {
     return true;
   }
   return false;
