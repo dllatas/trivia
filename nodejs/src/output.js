d795ca98482f078bcff8a2946e9079a57295c28f
Daniel Llatas Spiers <danllatas@gmail.com>
2019-08-25 21:17:38 +0200

    OUTPUT: Add NODE_ENV to avoid prints during testing

--- nodejs/src/output.js
+++ nodejs/src/output.js
@@ -2,38 +2,42 @@ const { EventEmitter } = require('events');
 
 const outputEmitter = new EventEmitter();
 
-const display = (message) => console.log(message);
+const display = (message) => {
+  if (process.env.NODE_ENV === 'dev') {
+    console.log(message);
+  }
+};
 
-outputEmitter.on('noPlayers', player => display(`There are no enough players`));
+outputEmitter.on('noPlayers', () => display('There are no enough players'));
 
-outputEmitter.on('newPlayer', player => display(`${player.name} was added`));
+outputEmitter.on('newPlayer', (player) => display(`${player.name} was added`));
 
-outputEmitter.on('winner', player => display(`${player.name} is the winner!!!`));
+outputEmitter.on('winner', (player) => display(`${player.name} is the winner!!!`));
 
-outputEmitter.on('totalPlayers', index => display(`They are player number ${index}`));
+outputEmitter.on('totalPlayers', (index) => display(`They are player number ${index}`));
 
-outputEmitter.on('currentPlayer', player => display(`${player.name} is the current player`));
+outputEmitter.on('currentPlayer', (player) => display(`${player.name} is the current player`));
 
-outputEmitter.on('penaltyOut', player => display(`${player.name} is getting out of the penalty box`));
+outputEmitter.on('penaltyOut', (player) => display(`${player.name} is getting out of the penalty box`));
 
-outputEmitter.on('penaltyStay', player => display(`${player.name} is not getting out of the penalty box`));
+outputEmitter.on('penaltyStay', (player) => display(`${player.name} is not getting out of the penalty box`));
 
-outputEmitter.on('penaltySent', player => display(`${player.name} was sent to the penalty box`));
+outputEmitter.on('penaltySent', (player) => display(`${player.name} was sent to the penalty box`));
 
 outputEmitter.on('movePlayer', (name, place) => display(`${name}'s new location is ${place}`));
 
-outputEmitter.on('coinsPlayer', player => display(`${player.name} now has ${player.purse} Gold Coins.`));
+outputEmitter.on('coinsPlayer', (player) => display(`${player.name} now has ${player.purse} Gold Coins.`));
 
-outputEmitter.on('askQuestion', question => display(question));
+outputEmitter.on('askQuestion', (question) => display(question));
 
-outputEmitter.on('roll', roll => display(`They have rolled a ${roll}`));
+outputEmitter.on('roll', (roll) => display(`They have rolled a ${roll}`));
 
-outputEmitter.on('currentCategory', category => display(`The category is ${category}`));
+outputEmitter.on('currentCategory', (category) => display(`The category is ${category}`));
 
 outputEmitter.on('correctAnswer', () => display('Answer was correct!!!!'));
 
 outputEmitter.on('wrongAnswer', () => display('Question was incorrectly answered'));
 
 module.exports = {
-  outputEmitter
-}
+  outputEmitter,
+};
